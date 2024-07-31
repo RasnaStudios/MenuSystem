24a4e94fbe50084c0f41787cc8163634ec59255b
Lorenzo Andraghetti <andraghetti.l@gmail.com>
2024-07-31 17:00:53 +0200

    Create functions, delegates and delegates handles

--- Plugins/MultiplayerSessions/Source/MultiplayerSessions/Private/MultiplayerSessionsSubsystem.cpp
+++ Plugins/MultiplayerSessions/Source/MultiplayerSessions/Private/MultiplayerSessionsSubsystem.cpp
@@ -1,3 +1,55 @@
 // Copyright (c) 2023-2024 Rasna Studios. All rights reserved.
 
 #include "MultiplayerSessionsSubsystem.h"
+
+#include "OnlineSubsystem.h"
+
+UMultiplayerSessionsSubsystem::UMultiplayerSessionsSubsystem()
+    : CreateSessionCompleteDelegate(FOnCreateSessionCompleteDelegate::CreateUObject(this, &ThisClass::OnCreateSessionComplete))
+    , FindSessionsCompleteDelegate(FOnFindSessionsCompleteDelegate::CreateUObject(this, &ThisClass::OnFindSessionsComplete))
+    , JoinSessionCompleteDelegate(FOnJoinSessionCompleteDelegate::CreateUObject(this, &ThisClass::OnJoinSessionComplete))
+    , DestroySessionCompleteDelegate(FOnDestroySessionCompleteDelegate::CreateUObject(this, &ThisClass::OnDestroySessionComplete))
+    , StartSessionCompleteDelegate(FOnStartSessionCompleteDelegate::CreateUObject(this, &ThisClass::OnStartSessionComplete))
+{
+    // Get the session interface from the online subsystem
+    if (const IOnlineSubsystem* Subsystem = IOnlineSubsystem::Get())
+    {
+        SessionInterface = Subsystem->GetSessionInterface();
+    }
+}
+
+// Functions to handle session functionalities
+
+void UMultiplayerSessionsSubsystem::CreateSession(int32 NumPublicConnections, FString MatchType)
+{
+}
+void UMultiplayerSessionsSubsystem::FindSessions(int32 MaxSearchResults)
+{
+}
+void UMultiplayerSessionsSubsystem::JoinSession(const FOnlineSessionSearchResult& SearchResult)
+{
+}
+void UMultiplayerSessionsSubsystem::DestroySession()
+{
+}
+void UMultiplayerSessionsSubsystem::StartSession()
+{
+}
+
+// Callbacks for delegates
+
+void UMultiplayerSessionsSubsystem::OnCreateSessionComplete(FName SessionName, bool bWasSuccessful)
+{
+}
+void UMultiplayerSessionsSubsystem::OnFindSessionsComplete(bool bWasSuccessful)
+{
+}
+void UMultiplayerSessionsSubsystem::OnJoinSessionComplete(FName SessionName, EOnJoinSessionCompleteResult::Type Result)
+{
+}
+void UMultiplayerSessionsSubsystem::OnDestroySessionComplete(FName SessionName, bool bWasSuccessful)
+{
+}
+void UMultiplayerSessionsSubsystem::OnStartSessionComplete(FName SessionName, bool bWasSuccessful)
+{
+}
--- Plugins/MultiplayerSessions/Source/MultiplayerSessions/Public/MultiplayerSessionsSubsystem.h
+++ Plugins/MultiplayerSessions/Source/MultiplayerSessions/Public/MultiplayerSessionsSubsystem.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Interfaces/OnlineSessionInterface.h"
 #include "Subsystems/GameInstanceSubsystem.h"
 
 #include "MultiplayerSessionsSubsystem.generated.h"
@@ -17,4 +18,49 @@ class MULTIPLAYERSESSIONS_API UMultiplayerSessionsSubsystem : public UGameInstan
 
 public:
     UMultiplayerSessionsSubsystem();
+
+    //
+    // Functions to handle session functionalities
+    // The Menu class will call these
+    //
+
+    void CreateSession(int32 NumPublicConnections, FString MatchType);
+    void FindSessions(int32 MaxSearchResults);
+    void JoinSession(const FOnlineSessionSearchResult& SearchResult);
+    void DestroySession();
+    void StartSession();
+
+protected:
+    // Internal callbacks for the delegates we will add to the Online Session Interface delegate list
+    // These don't need to be called outside of this class
+
+    void OnCreateSessionComplete(FName SessionName, bool bWasSuccessful);
+    void OnFindSessionsComplete(bool bWasSuccessful);
+    void OnJoinSessionComplete(FName SessionName, EOnJoinSessionCompleteResult::Type Result);
+    void OnDestroySessionComplete(FName SessionName, bool bWasSuccessful);
+    void OnStartSessionComplete(FName SessionName, bool bWasSuccessful);
+
+private:
+    IOnlineSessionPtr SessionInterface;
+
+    //
+    // To add to the Onlune Session Interface delegate list
+    // We will bind our MultiplayerSessionsSystem interal callbacks to these.
+    // For each of the delegates we need a delegate handle to add and remove them
+    //
+
+    FOnCreateSessionCompleteDelegate CreateSessionCompleteDelegate;
+    FDelegateHandle CreateSessionCompleteDelegateHandle;
+
+    FOnFindSessionsCompleteDelegate FindSessionsCompleteDelegate;
+    FDelegateHandle FindSessionsCompleteDelegateHandle;
+
+    FOnJoinSessionCompleteDelegate JoinSessionCompleteDelegate;
+    FDelegateHandle JoinSessionCompleteDelegateHandle;
+
+    FOnDestroySessionCompleteDelegate DestroySessionCompleteDelegate;
+    FDelegateHandle DestroySessionCompleteDelegateHandle;
+
+    FOnStartSessionCompleteDelegate StartSessionCompleteDelegate;
+    FDelegateHandle StartSessionCompleteDelegateHandle;
 };
